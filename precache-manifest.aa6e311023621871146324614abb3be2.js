be888a71c0f4314b1d8a44097ac93d4c8a9e536a
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-04-22 16:50:16 +0300

    Updates

---
+++ precache-manifest.aa6e311023621871146324614abb3be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfd8a33ac8fe990c1f58b36919b204f",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "0cabb046fa6823039a10",
+    "url": "/github-jobs/static/css/main.40b33fd8.chunk.css"
+  },
+  {
+    "revision": "c22f28026ee7328b6eeb",
+    "url": "/github-jobs/static/js/2.8cae2980.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github-jobs/static/js/2.8cae2980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cabb046fa6823039a10",
+    "url": "/github-jobs/static/js/main.86cbef90.chunk.js"
+  },
+  {
+    "revision": "385f2d0844dc63b89903",
+    "url": "/github-jobs/static/js/runtime-main.e8f71849.js"
+  }
+]);
